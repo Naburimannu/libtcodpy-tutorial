15af552a96654a02fff28a1c31921bd6d5694b88
Tom Hudson <tom.hudson.phd@gmail.com>
2016-02-02 22:14:15 -0500

    Break render_all() and other drawing functions out to renderer.py; encapsulate con and panel globals, but still a bit messy - particularly duplicated constants.

--- tutorial.py
+++ tutorial.py
@@ -10,6 +10,7 @@ import shelve
  
 from Tile import Tile
 from components import *
+from renderer import render_all, clear_object, clear_console, menu, renderer_init
  
 #actual size of the window
 SCREEN_WIDTH = 80
@@ -18,18 +19,16 @@ SCREEN_HEIGHT = 50
 #size of the map
 MAP_WIDTH = 80
 MAP_HEIGHT = 43
- 
-#sizes and coordinates relevant for the GUI
-BAR_WIDTH = 20
+
 PANEL_HEIGHT = 7
-PANEL_Y = SCREEN_HEIGHT - PANEL_HEIGHT
-MSG_X = BAR_WIDTH + 2
+BAR_WIDTH = 20
 MSG_WIDTH = SCREEN_WIDTH - BAR_WIDTH - 2
 MSG_HEIGHT = PANEL_HEIGHT - 1
 INVENTORY_WIDTH = 50
 CHARACTER_SCREEN_WIDTH = 30
 LEVEL_SCREEN_WIDTH = 40
- 
+
+  
 #parameters for dungeon generator
 ROOM_MAX_SIZE = 10
 ROOM_MIN_SIZE = 6
@@ -49,17 +48,10 @@ LEVEL_UP_BASE = 200
 LEVEL_UP_FACTOR = 150
  
  
-FOV_ALGO = 0  #default FOV algorithm
-FOV_LIGHT_WALLS = True  #light walls or not
-TORCH_RADIUS = 10
- 
-LIMIT_FPS = 20  #20 frames-per-second maximum
  
  
-color_dark_wall = libtcod.Color(0, 0, 100)
-color_light_wall = libtcod.Color(130, 110, 50)
-color_dark_ground = libtcod.Color(50, 50, 150)
-color_light_ground = libtcod.Color(200, 180, 50)
+ 
+
  
  
 class Rect:
@@ -529,115 +521,6 @@ def place_objects(room):
             item.always_visible = True  #items are visible even out-of-FOV, if in an explored area
  
  
-def render_bar(x, y, total_width, name, value, maximum, bar_color, back_color):
-    #render a bar (HP, experience, etc). first calculate the width of the bar
-    bar_width = int(float(value) / maximum * total_width)
- 
-    #render the background first
-    libtcod.console_set_default_background(panel, back_color)
-    libtcod.console_rect(panel, x, y, total_width, 1, False, libtcod.BKGND_SCREEN)
- 
-    #now render the bar on top
-    libtcod.console_set_default_background(panel, bar_color)
-    if bar_width > 0:
-        libtcod.console_rect(panel, x, y, bar_width, 1, False, libtcod.BKGND_SCREEN)
- 
-    #finally, some centered text with the values
-    libtcod.console_set_default_foreground(panel, libtcod.white)
-    libtcod.console_print_ex(panel, x + total_width / 2, y, libtcod.BKGND_NONE, libtcod.CENTER,
-                                 name + ': ' + str(value) + '/' + str(maximum))
- 
-def get_names_under_mouse():
-    global mouse
-    #return a string with the names of all objects under the mouse
- 
-    (x, y) = (mouse.cx, mouse.cy)
- 
-    #create a list with the names of all objects at the mouse's coordinates and in FOV
-    names = [obj.name for obj in objects
-             if obj.x == x and obj.y == y and libtcod.map_is_in_fov(fov_map, obj.x, obj.y)]
- 
-    names = ', '.join(names)  #join the names, separated by commas
-    return names.capitalize()
- 
-def draw_object(o):
-    #only show if it's visible to the player; or it's set to "always visible" and on an explored tile
-    if (libtcod.map_is_in_fov(fov_map, o.x, o.y) or
-            (o.always_visible and map[o.x][o.y].explored)):
-        #set the color and then draw the character that represents this object at its position
-        libtcod.console_set_default_foreground(con, o.color)
-        libtcod.console_put_char(con, o.x, o.y, o.char, libtcod.BKGND_NONE)
-
- 
-def clear_object(o):
-    #erase the character that represents this object
-    libtcod.console_put_char(con, o.x, o.y, ' ', libtcod.BKGND_NONE)
-
-def render_all():
-    global fov_map, color_dark_wall, color_light_wall
-    global color_dark_ground, color_light_ground
-    global fov_recompute
- 
-    if fov_recompute:
-        #recompute FOV if needed (the player moved or something)
-        fov_recompute = False
-        libtcod.map_compute_fov(fov_map, player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO)
- 
-        #go through all tiles, and set their background color according to the FOV
-        for y in range(MAP_HEIGHT):
-            for x in range(MAP_WIDTH):
-                visible = libtcod.map_is_in_fov(fov_map, x, y)
-                wall = map[x][y].block_sight
-                if not visible:
-                    #if it's not visible right now, the player can only see it if it's explored
-                    if map[x][y].explored:
-                        if wall:
-                            libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET)
-                        else:
-                            libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
-                else:
-                    #it's visible
-                    if wall:
-                        libtcod.console_set_char_background(con, x, y, color_light_wall, libtcod.BKGND_SET )
-                    else:
-                        libtcod.console_set_char_background(con, x, y, color_light_ground, libtcod.BKGND_SET )
-                        #since it's visible, explore it
-                    map[x][y].explored = True
- 
-    #draw all objects in the list, except the player. we want it to
-    #always appear over all other objects! so it's drawn later.
-    for object in objects:
-        if object != player:
-            draw_object(object)
-    draw_object(player)
- 
-    #blit the contents of "con" to the root console
-    libtcod.console_blit(con, 0, 0, MAP_WIDTH, MAP_HEIGHT, 0, 0, 0)
- 
- 
-    #prepare to render the GUI panel
-    libtcod.console_set_default_background(panel, libtcod.black)
-    libtcod.console_clear(panel)
- 
-    #print the game messages, one line at a time
-    y = 1
-    for (line, color) in game_msgs:
-        libtcod.console_set_default_foreground(panel, color)
-        libtcod.console_print_ex(panel, MSG_X, y, libtcod.BKGND_NONE, libtcod.LEFT,line)
-        y += 1
- 
-    #show the player's stats
-    render_bar(1, 1, BAR_WIDTH, 'HP', player.fighter.hp, player.fighter.max_hp,
-               libtcod.light_red, libtcod.darker_red)
-    libtcod.console_print_ex(panel, 1, 3, libtcod.BKGND_NONE, libtcod.LEFT, 'Dungeon level ' + str(dungeon_level))
- 
-    #display names of objects under the mouse
-    libtcod.console_set_default_foreground(panel, libtcod.light_gray)
-    libtcod.console_print_ex(panel, 1, 0, libtcod.BKGND_NONE, libtcod.LEFT, get_names_under_mouse())
- 
-    #blit the contents of "panel" to the root console
-    libtcod.console_blit(panel, 0, 0, SCREEN_WIDTH, PANEL_HEIGHT, 0, 0, PANEL_Y)
- 
  
 def message(new_msg, color = libtcod.white):
     #split the message if necessary, among multiple lines
@@ -673,49 +556,7 @@ def player_move_or_attack(dx, dy):
         move(player, dx, dy)
         fov_recompute = True
  
- 
-def menu(header, options, width):
-    if len(options) > 26: raise ValueError('Cannot have a menu with more than 26 options.')
- 
-    #calculate total height for the header (after auto-wrap) and one line per option
-    header_height = libtcod.console_get_height_rect(con, 0, 0, width, SCREEN_HEIGHT, header)
-    if header == '':
-        header_height = 0
-    height = len(options) + header_height
- 
-    #create an off-screen console that represents the menu's window
-    window = libtcod.console_new(width, height)
- 
-    #print the header, with auto-wrap
-    libtcod.console_set_default_foreground(window, libtcod.white)
-    libtcod.console_print_rect_ex(window, 0, 0, width, height, libtcod.BKGND_NONE, libtcod.LEFT, header)
- 
-    #print all the options
-    y = header_height
-    letter_index = ord('a')
-    for option_text in options:
-        text = '(' + chr(letter_index) + ') ' + option_text
-        libtcod.console_print_ex(window, 0, y, libtcod.BKGND_NONE, libtcod.LEFT, text)
-        y += 1
-        letter_index += 1
- 
-    #blit the contents of "window" to the root console
-    x = SCREEN_WIDTH/2 - width/2
-    y = SCREEN_HEIGHT/2 - height/2
-    libtcod.console_blit(window, 0, 0, width, height, 0, x, y, 1.0, 0.7)
- 
-    #present the root console to the player and wait for a key-press
-    libtcod.console_flush()
-    key = libtcod.console_wait_for_keypress(True)
- 
-    if key.vk == libtcod.KEY_ENTER and key.lalt:  #(special case) Alt+Enter: toggle fullscreen
-        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen)
- 
-    #convert the ASCII code to an index; if it corresponds to an option, return it
-    index = key.c - ord('a')
-    if index >= 0 and index < len(options): return index
-    return None
- 
+
 def inventory_menu(header):
     #show a menu with each item of the inventory as an option
     if len(inventory) == 0:
@@ -859,7 +700,7 @@ def target_tile(max_range=None):
         #render the screen. this erases the inventory and shows the names of objects under the mouse.
         libtcod.console_flush()
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
-        render_all()
+        render_all(fov_recompute, fov_map, map, objects, player, dungeon_level, game_msgs, mouse)
  
         (x, y) = (mouse.cx, mouse.cy)
  
@@ -1024,7 +865,7 @@ def initialize_fov():
         for x in range(MAP_WIDTH):
             libtcod.map_set_properties(fov_map, x, y, not map[x][y].block_sight, not map[x][y].blocked)
  
-    libtcod.console_clear(con)  #unexplored areas start black (which is the default background color)
+    clear_console()  #unexplored areas start black (which is the default background color)
  
 def play_game():
     global key, mouse
@@ -1036,8 +877,7 @@ def play_game():
     #main loop
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
-        #render the screen
-        render_all()
+        render_all(fov_recompute, fov_map, map, objects, player, dungeon_level, game_msgs, mouse)
  
         libtcod.console_flush()
  
@@ -1089,10 +929,6 @@ def main_menu():
         elif choice == 2:  #quit
             break
  
-libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
-libtcod.sys_set_fps(LIMIT_FPS)
-con = libtcod.console_new(MAP_WIDTH, MAP_HEIGHT)
-panel = libtcod.console_new(SCREEN_WIDTH, PANEL_HEIGHT)
+renderer_init()
  
 main_menu()
