1c7ad3aa908923519428f979a968eb23b4cc54d8
Tom Hudson <tom.hudson.phd@gmail.com>
2016-02-04 19:02:21 -0500

    Add file spells.py.
    In addition to spell definition also has targeting routines and closest_monster(), at least for now.
    BUG?: save_game() is getting permissions errors attempting to open the file.

---
+++ spells.py
@@ -0,0 +1,107 @@
+import libtcodpy as libtcod
+
+import log
+import components
+import actions
+import ui
+ 
+#spell values
+HEAL_AMOUNT = 40
+LIGHTNING_DAMAGE = 40
+LIGHTNING_RANGE = 5
+CONFUSE_RANGE = 8
+FIREBALL_RADIUS = 3
+FIREBALL_DAMAGE = 25
+
+def target_tile(actor, max_range=None):
+    """
+    Return the position of a tile left-clicked in player's FOV (optionally in a range), or (None,None) if right-clicked.
+    """
+    while True:
+        # Render the screen. This erases the inventory and shows the names of objects under the mouse.
+        libtcod.console_flush()
+        ui.poll()
+        renderer.render_all(actor, ui.mouse)
+        actor.current_map.fov_needs_recompute = False
+ 
+        (x, y) = (ui.mouse.cx, ui.mouse.cy)
+ 
+        if ui.mouse.rbutton_pressed or ui.key.vk == libtcod.KEY_ESCAPE:
+            return (None, None)
+ 
+        #accept the target if the player clicked in FOV, and in case a range is specified, if it's in that range
+        if (ui.mouse.lbutton_pressed and libtcod.map_is_in_fov(actor.current_map.fov_map, x, y) and
+                (max_range is None or actor.distance(x, y) <= max_range)):
+            return (x, y)
+ 
+def target_monster(actor, max_range=None):
+    #returns a clicked monster inside FOV up to a range, or None if right-clicked
+    while True:
+        (x, y) = target_tile(actor, max_range)
+        if x is None:  #player cancelled
+            return None
+ 
+        #return the first clicked monster, otherwise continue looping
+        for obj in actor.current_map.objects:
+            if obj.x == x and obj.y == y and obj.fighter and obj != actor:
+                return obj
+ 
+def closest_monster(actor, max_range):
+    #find closest enemy, up to a maximum range, and in the player's FOV
+    closest_enemy = None
+    closest_dist = max_range + 1  #start with (slightly more than) maximum range
+ 
+    for object in actor.current_map.objects:
+        if object.fighter and not object == actor and libtcod.map_is_in_fov(actor.current_map.fov_map, object.x, object.y):
+            dist = actor.distance_to(object)
+            if dist < closest_dist:
+                closest_enemy = object
+                closest_dist = dist
+    return closest_enemy
+ 
+def cast_heal(actor):
+    """
+    Heal the caster.
+    """
+    if actor.fighter.hp == actor.fighter.max_hp:
+        log.message('You are already at full health.', libtcod.red)
+        return 'cancelled'
+ 
+    log.message('Your wounds start to feel better!', libtcod.light_violet)
+    actions.heal(actor.fighter, HEAL_AMOUNT)
+ 
+def cast_lightning(actor):
+    #find closest enemy (inside a maximum range) and damage it
+    monster = closest_monster(actor, LIGHTNING_RANGE)
+    if monster is None:  #no enemy found within maximum range
+        log.message('No enemy is close enough to strike.', libtcod.red)
+        return 'cancelled'
+ 
+    #zap it!
+    log.message('A lighting bolt strikes the ' + monster.name + ' with a loud thunder! The damage is '
+            + str(LIGHTNING_DAMAGE) + ' hit points.', libtcod.light_blue)
+    actions.inflict_damage(actor, monster.fighter, LIGHTNING_DAMAGE)
+ 
+def cast_fireball(actor):
+    log.message('Left-click a target tile for the fireball, or right-click to cancel.', libtcod.light_cyan)
+    (x, y) = target_tile(actor)
+    if x is None: return 'cancelled'
+    log.message('The fireball explodes, burning everything within ' + str(FIREBALL_RADIUS) + ' tiles!', libtcod.orange)
+ 
+    for obj in actor.current_map.objects:
+        if obj.distance(x, y) <= FIREBALL_RADIUS and obj.fighter:
+            log.message('The ' + obj.name + ' gets burned for ' + str(FIREBALL_DAMAGE) + ' hit points.', libtcod.orange)
+            actions.inflict_damage(actor, obj.fighter, FIREBALL_DAMAGE)
+ 
+def cast_confuse(actor):
+    log.message('Left-click an enemy to confuse it, or right-click to cancel.', libtcod.light_cyan)
+    monster = target_monster(actor, CONFUSE_RANGE)
+    if monster is None: return 'cancelled'
+ 
+    old_ai = monster.ai
+    monster.ai = AI(ai.confused_monster, ai.confused_monster_metadata(old_ai))
+    monster.ai.set_owner(monster)
+    log.message('The eyes of the ' + monster.name + ' look vacant, as he starts to stumble around!', libtcod.light_green)
+ 
+ 
+ 
--- tutorial.py
+++ tutorial.py
@@ -17,6 +17,8 @@ import actions
 import ai
 import ui
 
+import spells
+
 INVENTORY_WIDTH = 50
 CHARACTER_SCREEN_WIDTH = 30
 LEVEL_SCREEN_WIDTH = 40
@@ -27,13 +29,6 @@ ROOM_MAX_SIZE = 10
 ROOM_MIN_SIZE = 6
 MAX_ROOMS = 30
  
-#spell values
-HEAL_AMOUNT = 40
-LIGHTNING_DAMAGE = 40
-LIGHTNING_RANGE = 5
-CONFUSE_RANGE = 8
-FIREBALL_RADIUS = 3
-FIREBALL_DAMAGE = 25
  
 #experience and level-ups
 LEVEL_UP_BASE = 200
@@ -337,22 +332,22 @@ def place_objects(new_map, room):
             choice = random_choice(item_chances)
             if choice == 'heal':
                 #create a healing potion
-                item_component = Item(use_function=cast_heal)
+                item_component = Item(use_function=spells.cast_heal)
                 item = Object(x, y, '!', 'healing potion', libtcod.violet, item=item_component)
  
             elif choice == 'lightning':
                 #create a lightning bolt scroll
-                item_component = Item(use_function=cast_lightning)
+                item_component = Item(use_function=spells.cast_lightning)
                 item = Object(x, y, '#', 'scroll of lightning bolt', libtcod.light_yellow, item=item_component)
  
             elif choice == 'fireball':
                 #create a fireball scroll
-                item_component = Item(use_function=cast_fireball)
+                item_component = Item(use_function=spells.cast_fireball)
                 item = Object(x, y, '#', 'scroll of fireball', libtcod.light_yellow, item=item_component)
  
             elif choice == 'confuse':
                 #create a confuse scroll
-                item_component = Item(use_function=cast_confuse)
+                item_component = Item(use_function=spells.cast_confuse)
                 item = Object(x, y, '#', 'scroll of confusion', libtcod.light_yellow, item=item_component)
  
             elif choice == 'sword':
@@ -523,97 +518,6 @@ def monster_death(monster):
     monster.name = 'remains of ' + monster.name
     monster.current_map.objects.remove(monster)
     monster.current_map.objects.insert(0, monster)
- 
-def target_tile(actor, max_range=None):
-    """
-    Return the position of a tile left-clicked in player's FOV (optionally in a range), or (None,None) if right-clicked.
-    """
-    while True:
-        # Render the screen. This erases the inventory and shows the names of objects under the mouse.
-        libtcod.console_flush()
-        ui.poll()
-        renderer.render_all(actor, ui.mouse)
-        actor.current_map.fov_needs_recompute = False
- 
-        (x, y) = (ui.mouse.cx, ui.mouse.cy)
- 
-        if ui.mouse.rbutton_pressed or ui.key.vk == libtcod.KEY_ESCAPE:
-            return (None, None)
- 
-        #accept the target if the player clicked in FOV, and in case a range is specified, if it's in that range
-        if (ui.mouse.lbutton_pressed and libtcod.map_is_in_fov(actor.current_map.fov_map, x, y) and
-                (max_range is None or actor.distance(x, y) <= max_range)):
-            return (x, y)
- 
-def target_monster(actor, max_range=None):
-    #returns a clicked monster inside FOV up to a range, or None if right-clicked
-    while True:
-        (x, y) = target_tile(actor, max_range)
-        if x is None:  #player cancelled
-            return None
- 
-        #return the first clicked monster, otherwise continue looping
-        for obj in actor.current_map.objects:
-            if obj.x == x and obj.y == y and obj.fighter and obj != actor:
-                return obj
- 
-def closest_monster(actor, max_range):
-    #find closest enemy, up to a maximum range, and in the player's FOV
-    closest_enemy = None
-    closest_dist = max_range + 1  #start with (slightly more than) maximum range
- 
-    for object in actor.current_map.objects:
-        if object.fighter and not object == actor and libtcod.map_is_in_fov(actor.current_map.fov_map, object.x, object.y):
-            dist = actor.distance_to(object)
-            if dist < closest_dist:
-                closest_enemy = object
-                closest_dist = dist
-    return closest_enemy
- 
-def cast_heal(actor):
-    """
-    Heal the caster.
-    """
-    if actor.fighter.hp == actor.fighter.max_hp:
-        log.message('You are already at full health.', libtcod.red)
-        return 'cancelled'
- 
-    log.message('Your wounds start to feel better!', libtcod.light_violet)
-    actions.heal(actor.fighter, HEAL_AMOUNT)
- 
-def cast_lightning(actor):
-    #find closest enemy (inside a maximum range) and damage it
-    monster = closest_monster(actor, LIGHTNING_RANGE)
-    if monster is None:  #no enemy found within maximum range
-        log.message('No enemy is close enough to strike.', libtcod.red)
-        return 'cancelled'
- 
-    #zap it!
-    log.message('A lighting bolt strikes the ' + monster.name + ' with a loud thunder! The damage is '
-            + str(LIGHTNING_DAMAGE) + ' hit points.', libtcod.light_blue)
-    actions.inflict_damage(actor, monster.fighter, LIGHTNING_DAMAGE)
- 
-def cast_fireball(actor):
-    log.message('Left-click a target tile for the fireball, or right-click to cancel.', libtcod.light_cyan)
-    (x, y) = target_tile(actor)
-    if x is None: return 'cancelled'
-    log.message('The fireball explodes, burning everything within ' + str(FIREBALL_RADIUS) + ' tiles!', libtcod.orange)
- 
-    for obj in actor.current_map.objects:
-        if obj.distance(x, y) <= FIREBALL_RADIUS and obj.fighter:
-            log.message('The ' + obj.name + ' gets burned for ' + str(FIREBALL_DAMAGE) + ' hit points.', libtcod.orange)
-            actions.inflict_damage(actor, obj.fighter, FIREBALL_DAMAGE)
- 
-def cast_confuse(actor):
-    log.message('Left-click an enemy to confuse it, or right-click to cancel.', libtcod.light_cyan)
-    monster = target_monster(actor, CONFUSE_RANGE)
-    if monster is None: return 'cancelled'
- 
-    old_ai = monster.ai
-    monster.ai = AI(ai.confused_monster, ai.confused_monster_metadata(old_ai))
-    monster.ai.set_owner(monster)
-    log.message('The eyes of the ' + monster.name + ' look vacant, as he starts to stumble around!', libtcod.light_green)
- 
  
 def save_game():
     global current_map, player, game_state
